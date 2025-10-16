e4dfd75a1bcc3bf41ddf445b90e539b0703a55ce
JevonTwitty <twittyjevon@gmail.com>
2025-10-16 13:10:48 -0400

    Solar dataset implementation

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,9 +1,10 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, Dataset_Solar
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
 data_dict = {
+    'solar': Dataset_Solar,
     'ETTh1': Dataset_ETT_hour,
     'ETTh2': Dataset_ETT_hour,
     'ETTm1': Dataset_ETT_minute,
